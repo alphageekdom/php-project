8682a00e3952df6a8b62aa13ac2c5ff21b3ee578
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-08 14:07:21 -0800

    Registration validation and error partials

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -30,4 +30,53 @@ class UserController {
     public function create() {
         loadView('users/create');
     }
+
+    /**
+     * Store user in database
+     * 
+     * @return void
+     */
+    public function store() {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+        $password = $_POST['password'];
+        $passwordConfirmation = $_POST['password_confirmation'];
+
+        $errors = [];
+
+        // Validation
+        if (!Validation::email($email)) {
+            $errors['email'] = 'Please enter a valid email address';
+        }
+
+        if (!Validation::string($name, 2, 50)) {
+            $errors['name'] = 'Name must be between 2 to 50 characters';
+        }
+
+        if (!Validation::string($name, 6, 50)) {
+            $errors['password'] = 'Password must be between 6 characters';
+        }
+
+
+        if (!Validation::match($password, $passwordConfirmation)) {
+            $errors['password_confirmation'] = 'Passwords do no match';
+        }
+
+        if (!empty($errors)) {
+            loadView('users/create', [
+                'errors' => $errors,
+                'user' => [
+                    'name' => $name,
+                    'email' => $email,
+                    'city' => $city,
+                    'state' => $state
+                ]
+            ]);
+            exit;
+        }
+
+        inspectAndDie('Store');
+    }
 }
