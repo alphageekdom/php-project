e5c5dfdd6233fcee848b1ff59799706b110b5d50
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-03-01 17:32:31 -0800

    Changed App Name

--- App/views/partials/head.php
+++ App/views/partials/head.php
@@ -8,7 +8,7 @@
     <meta name="description" content="Discover top-notch tech roles and diverse job opportunities on our specialized job posting site. Find your ideal career in technology and beyond with ease. Start your job search today!">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/css/style.css" />
-    <title>GeekGrid</title>
+    <title>PulseTech</title>
 </head>
 
 <body class="bg-gray-100">
